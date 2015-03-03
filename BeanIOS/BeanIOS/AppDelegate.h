9143bf3fa2976cf3aeb6eac3ebd0efae648863bf
Andrew McKnight <andrew@Andrews-MacBook-Pro.local>
2015-03-02 21:31:17 -0700

    First commit of test ios project

---
+++ BeanIOS/BeanIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeanIOS
+//
+//  Created by Andrew McKnight on 3/2/15.
+//  Copyright (c) 2015 Andrew McKnight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeanIOS/BeanIOSTests/BeanIOSTests.m
@@ -0,0 +1,40 @@
+//
+//  BeanIOSTests.m
+//  BeanIOSTests
+//
+//  Created by Andrew McKnight on 3/2/15.
+//  Copyright (c) 2015 Andrew McKnight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BeanIOSTests : XCTestCase
+
+@end
+
+@implementation BeanIOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
